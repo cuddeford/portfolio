d9b5da01da5a02e4e201d244918f1b610a22239e
Lucio Cuddeford <cuddeford@me.com>
2025-07-18 23:35:33 +0100

    Slightly reduce the bounce and make categories clearer

--- client/src/components/Portfolio/ProjectItems/index.tsx
+++ client/src/components/Portfolio/ProjectItems/index.tsx
@@ -159,7 +159,7 @@ const CategorySection = props => {
                         size='xs'
                         fixedWidth
                         rotation={iAmSelected ? 180 : 0}
-                        style={{ transition: 'transform 1000ms cubic-bezier(0.68, -0.55, 0.265, 1.55)' }}
+                        style={{ transition: 'transform 1000ms var(--bounce-curve)' }}
                     />
                 </span>
             </h4>
